7fd0b7f69bd81b4e11a660f1165acc2954887ad2
Oselskyi Oleksandr <oselskyi3@gmail.com>
2024-01-28 16:53:01 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 import io from 'socket.io-client';
 import './App.css';
